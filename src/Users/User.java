24b65466c336818ac3e33b682b7da9285b1fe0b1
HSH2006 <hossein.shakiba85@gmail.com>
2026-01-30 19:07:51 +0330

    Create User.java

---
+++ src/Users/User.java
@@ -0,0 +1,58 @@
+package Users;
+
+public abstract class User {
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    public User(String id, String username, String password, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
