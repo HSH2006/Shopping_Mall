f5e80339f09a0cb19a91339a74d0ab67f4ea30af
HSH2006 <hossein.shakiba85@gmail.com>
2026-02-02 16:16:03 +0330

    Create ShoppingCart.java

---
+++ src/Shopping/ShoppingCart.java
@@ -0,0 +1,58 @@
+package Shopping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private List<ProductInShoppingCart> products;
+    private double price;
+
+    public ShoppingCart() {
+        this.products = new ArrayList<>();
+        this.price = 0.0;
+    }
+
+    public void add(Product product) {
+        if (product.getNumber() == 0) {
+            throw new IllegalArgumentException("Mojodi nadrim.");
+        }
+
+        for (ProductInShoppingCart product2 : products)
+            if (product2.getProduct().getId().equals(product.getId())) {
+                product2.add();
+                calculatePrice();
+                return;
+            }
+
+        ProductInShoppingCart newProduct = new ProductInShoppingCart(product);
+        products.add(newProduct);
+        calculatePrice();
+    }
+
+    public void remove(String id) {
+        for (ProductInShoppingCart product : products)
+            if (product.getProduct().getId().equals(id)) {
+                product.remove();
+                calculatePrice();
+                return;
+            }
+    }
+
+    public void clear() {
+        products.clear();
+        price = 0.0;
+    }
+
+    private void calculatePrice() {
+        price = 0.0;
+        for (ProductInShoppingCart product : products)
+                price += ((double) product.getNumberInShoppingCart()) * product.getProduct().getPrice();
+    }
+
+    public List<ProductInShoppingCart> view() {
+        return products;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
