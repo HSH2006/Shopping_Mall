f568dbfb3de956f0ca0ba4b424d33acbcf2ca25a
HSH2006 <hossein.shakiba85@gmail.com>
2026-02-06 18:49:01 +0330

    Update Administrator.java

--- src/Users/Administrator.java
+++ src/Users/Administrator.java
@@ -1,8 +1,17 @@
 package Users;
 
+import Shopping.Product;
+
+import javax.swing.*;
+
 public class Administrator extends User {
 
-    public Administrator(String id, String username, String password, String email) {
-        super(id, username, password, email, "Administrator");
+    public Administrator(String username, String password, String email) {
+        super(username, password, email, "Administrator");
+    }
+
+    public void addProduct(String id, String name, String category, double price, int number, String title, ImageIcon image) {
+        Product newProduct = new Product(id, name, category, price, number, title, image);
+
     }
 }
