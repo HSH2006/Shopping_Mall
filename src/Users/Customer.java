ca35224015a1110a5a21068ace9a5476c7e22819
HSH2006 <hossein.shakiba85@gmail.com>
2026-02-06 18:49:49 +0330

    Update Customer.java

--- src/Users/Customer.java
+++ src/Users/Customer.java
@@ -1,11 +1,13 @@
 package Users;
 
+import Shopping.ShoppingCart;
+
 public class Customer extends User {
    private ShoppingCart cart;
 
 
-    public Customer(String id, String username, String password, String email) {
-        super(id, username, password, email, "Customer");
+    public Customer(String username, String password, String email) {
+        super(username, password, email, "Customer");
         this.cart = new ShoppingCart();
     }
 
